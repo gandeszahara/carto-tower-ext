884023764f9124c7874139ea77015a9ae6416b25
gandeszahara <firdausizaharagandes@gmail.com>
2025-01-17 12:06:46 +0700

    modify column naming in full run query & change version

--- components/nearest_distance_multi/src/dryrun.sql
+++ components/nearest_distance_multi/src/dryrun.sql
@@ -3,7 +3,10 @@
 
 EXECUTE IMMEDIATE '
 CREATE TABLE IF NOT EXISTS ' || :output_table || '
-AS SELECT *, 100 as distance
+AS SELECT *, 
+"POINT (174.7768961833 -41.2840625333)" as second_geom,
+"xx" as second_id,
+100 as distance
 FROM ' || :input_main_table || '
 WHERE 1 = 0;
 ';
--- components/nearest_distance_multi/src/fullrun.sql
+++ components/nearest_distance_multi/src/fullrun.sql
@@ -2,39 +2,32 @@
 ---------------------------------------------------------
 
 EXECUTE IMMEDIATE '
-CREATE TABLE IF NOT EXISTS ' || :output_table || '
-AS (
-
-WITH nearest AS (
-    SELECT                          
-        a.' || :geom_main_table|| ' AS MAIN_GEOM,                               -- Geometry of the main table
-        b.' || :geom_second_table|| ' AS SECOND_GEOM,                           -- Geometry of the second table
-        a.' || :id_main_table|| ' AS MAIN_ID,                                    -- ID of the main table
-        b.' || :id_second_table|| ' AS SECOND_ID,                               -- ID of the second table
-        a.* EXCLUDE(' || :geom_main_table|| ', ' || :id_main_table|| '),        -- Attributes from the main table
-        b.* EXCLUDE(' || :geom_second_table|| ', ' || :id_second_table|| '),    -- Attributes from the second table
-        ST_DISTANCE(a.' || :geom_main_table|| ', b.' || :geom_second_table|| ') AS DISTANCE -- Calculate the distance between first and second table
-    FROM 
-        ' || :input_main_table || ' a
-    JOIN 
-        ' || :input_second_table || ' b 
-    ON 
-        ST_DWITHIN(a.' || :geom_main_table|| ', b.' || :geom_second_table|| ', ' || :radius|| ') -- Ensures the distance between address and flood point is within defined radius
-),
-rank AS (
-    SELECT 
-        *,
-        ROW_NUMBER() OVER (PARTITION BY MAIN_ID ORDER BY DISTANCE) AS RANK_NUM -- Rank second points by distance
-    FROM 
-        nearest
-)
-SELECT 
-    * EXCLUDE (RANK_NUM)
-FROM 
-    rank
-WHERE 
-    RANK_NUM <= ' || :number_result|| ' -- Select only the X nearest secondary points for each main points
-ORDER BY 
-    MAIN_ID, RANK_NUM -- Order by address and distance rank
-)
-'
+  CREATE TABLE IF NOT EXISTS OR REPLACE TABLE ' || :output_table || ' AS
+    WITH nearest AS (
+        SELECT
+            a.' || :geom_main_table|| ',
+            b.' || :geom_second_table|| ' AS SECOND_GEOM,
+            a.' || :id_main_table|| ',
+            b.' || :id_second_table|| ' AS SECOND_ID,
+            a.* EXCLUDE(' || :geom_main_table|| ', ' || :id_main_table|| '),
+            b.* EXCLUDE(' || :geom_second_table|| ', ' || :id_second_table|| '),
+            ST_DISTANCE(a.' || :geom_main_table|| ', b.' || :geom_second_table|| ') AS DISTANCE
+        FROM ' || :input_main_table || ' a
+        JOIN ' || :input_second_table || ' b
+        ON ST_DWITHIN(a.' || :geom_main_table|| ', b.' || :geom_second_table|| ', ' || :radius|| ')
+    ),
+    rank AS (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY nearest.' || :id_main_table|| ' ORDER BY DISTANCE) AS RANK_NUM
+        FROM
+            nearest
+    )
+    SELECT
+        * EXCLUDE (RANK_NUM)
+    FROM
+        rank
+    WHERE
+        RANK_NUM <= ' || :number_result|| '
+    ORDER BY
+       rank.' || :id_main_table|| ', RANK_NUM';
